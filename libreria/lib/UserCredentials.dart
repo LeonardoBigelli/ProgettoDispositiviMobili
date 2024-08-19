3e053b077782ac7bce4a25d3dee3aefba7c5957e
LeonardoBigelli <l.bigelli2@campus.uniurb.it>
2024-08-19 17:47:10 +0200

    Aggiunta lista dei preferiti
    Aggiunta relazione libri preferiti - utente

--- libreria/lib/UserCredentials.dart
+++ libreria/lib/UserCredentials.dart
@@ -1,11 +1,27 @@
 class UserCredentials {
   String username;
   String password;
+  List<Map<String, String>> favoriteBooks;
 
-  UserCredentials({required this.username, required this.password});
+  UserCredentials(
+      {required this.username,
+      required this.password,
+      this.favoriteBooks = const []});
 
   bool validate() {
     //per ora è presente solamente l'utente admin
     return username == "admin" && password == "admin";
+  }
+
+  void addFavoriteBook(String title, String author, String coverUrl) {
+    favoriteBooks.add({
+      'title': title,
+      'author': author,
+      'coverUrl': coverUrl,
+    });
+  }
+
+  void removeFavoriteBook(String title) {
+    favoriteBooks.removeWhere((book) => book['title'] == title);
   }
 }
