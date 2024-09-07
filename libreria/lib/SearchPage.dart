21927c4fc5b015618f40da3aa4e807f5ea223c8e
LeonardoBigelli <l.bigelli2@campus.uniurb.it>
2024-09-07 16:09:30 +0200

    Update SearchPage.dart
    risolti numerosi bug per la gestione della rotazione.

--- libreria/lib/SearchPage.dart
+++ libreria/lib/SearchPage.dart
@@ -4,7 +4,6 @@ import 'package:libreria/BookDetailsPage.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-//NON SONO SICURO CHE SERVA LO STATO (DA RIVEDERE), per ora serve (lo stato) per capire se ho caricato i libri o meno
 class SearchPage extends ConsumerStatefulWidget {
   @override
   _SearchPageState createState() => _SearchPageState();
@@ -46,70 +45,59 @@ class _SearchPageState extends ConsumerState<SearchPage> {
       appBar: AppBar(
         title: const Text('Cerca Libri', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.indigo,
-        //sezione che precede il titolo, per tornare alla pagina precedente
-        /* leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),*/
       ),
-      body: Padding(
+      body: ListView(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _searchController,
-              decoration: const InputDecoration(
-                labelText: 'Cerca un libro',
-                border: OutlineInputBorder(),
-              ),
-              onSubmitted: (_) => _searchBooks(),
+        children: [
+          TextField(
+            controller: _searchController,
+            decoration: const InputDecoration(
+              hintText: 'Cerca un libro',
+              border: OutlineInputBorder(),
             ),
-            const SizedBox(height: 16),
-            _isLoading
-                ? const CircularProgressIndicator()
-                : Expanded(
-                    //ListView per mostrare tutti i libri caricati
-                    child: ListView.builder(
-                      itemCount: _books.length,
-                      itemBuilder: (context, index) {
-                        final book = _books[index];
-                        final title = book['title'] ?? 'Titolo non disponibile';
-                        final author = book['author_name']?.join(', ') ??
-                            'Autore non disponibile';
-                        final coverUrl = book['cover_i'] != null
+            onSubmitted: (_) => _searchBooks(),
+          ),
+          const SizedBox(height: 16),
+          if (_isLoading)
+            Center(
+              child: SizedBox(
+                height: 50,
+                width: 50,
+                child: CircularProgressIndicator(),
+              ),
+            )
+          else ...[
+            for (var book in _books)
+              ListTile(
+                leading: book['cover_i'] != null
+                    ? Image.network(
+                        'https://covers.openlibrary.org/b/id/${book['cover_i']}-M.jpg',
+                        width: 50,
+                        fit: BoxFit.cover,
+                      )
+                    : const Icon(Icons.book),
+                title: Text(book['title'] ?? 'Titolo non disponibile'),
+                subtitle: Text(book['author_name']?.join(', ') ??
+                    'Autore non disponibile'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => BookDetailsPage(
+                        title: book['title'] ?? 'Titolo non disponibile',
+                        author: book['author_name']?.join(', ') ??
+                            'Autore non disponibile',
+                        coverUrl: book['cover_i'] != null
                             ? 'https://covers.openlibrary.org/b/id/${book['cover_i']}-M.jpg'
-                            : '';
-
-                        return ListTile(
-                          leading: coverUrl.isNotEmpty
-                              ? Image.network(coverUrl,
-                                  width: 50, fit: BoxFit.cover)
-                              : const Icon(Icons.book),
-                          title: Text(title),
-                          subtitle: Text(author),
-                          onTap: () {
-                            // Naviga alla pagina dei dettagli del libro
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => BookDetailsPage(
-                                  title: title,
-                                  author: author,
-                                  coverUrl:
-                                      'https://covers.openlibrary.org/b/id/${book['cover_i']}-M.jpg',
-                                  description: book['description'] ?? '',
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      },
+                            : '',
+                        description: book['description'] ?? '',
+                      ),
                     ),
-                  ),
+                  );
+                },
+              ),
           ],
-        ),
+        ],
       ),
     );
   }
